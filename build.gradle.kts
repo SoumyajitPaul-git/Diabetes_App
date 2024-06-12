2d9c68927b52ce769ab35523c0372e072bbb1530
Soumyajit Paul <soumyajit-paul-br22-3028@gcect.ac.in>
2024-06-12 15:43:34 +0530

    Normalize all the line endings

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+dependencies{
+    classpath 'com.google.gms:google-services:4.3.10'
+}
