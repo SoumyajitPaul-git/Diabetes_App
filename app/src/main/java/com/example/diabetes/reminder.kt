3a23679a418c7d40d8f26eede2be104622797a0d
Soumyajit Paul <146088614+SoumyajitPaul-git@users.noreply.github.com>
2024-07-10 01:54:21 +0530

    Files removed

--- app/src/main/java/com/example/diabetes/reminder.kt
+++
@@ -1,4 +0,0 @@
-package com.example.diabetes
-
-class reminder {
-}
--- app/src/main/java/com/example/diabetes/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.diabetes.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
